2794c990ec0ced8f563399e6cce42f63a1b71be2
JayRoth-sch <35668950+JayRothenberger@users.noreply.github.com>
2023-10-20 23:51:08 -0500

    adding a few comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torchvision
 from torchvision.transforms import v2
 from util import accuracy, RepeatLoader
 from mpl import MPL
-
+torch.manual_seed(42)
 transform = v2.Compose(
                     [
                         torchvision.transforms.Resize(size=(28, 28), antialias=True),
@@ -14,7 +14,7 @@ transform = v2.Compose(
 train = torchvision.datasets.MNIST('/scratch/jroth/mnist/', True, download=True,transform =transform)
 val = torchvision.datasets.MNIST('/scratch/jroth/mnist/', False, download=True, transform =transform)
 
-train_kwargs = {'batch_size': 128}
+train_kwargs = {'batch_size': 256}
 
 cuda_kwargs = {'num_workers': 12,
                 'pin_memory': True,
@@ -24,16 +24,16 @@ train_kwargs.update(cuda_kwargs)
 
 train_loader = torch.utils.data.DataLoader(train, **train_kwargs)
 val_loader = torch.utils.data.DataLoader(val, **train_kwargs)
-
+# data loaders that repeat forever
 train = RepeatLoader(train_loader)
 val = RepeatLoader(val_loader)
-
+# define the two models
 student = torchvision.ops.MLP(784, [512, 256, 10], torch.nn.BatchNorm1d, torch.nn.ReLU, True, 0.1)
 teacher = torchvision.ops.MLP(784, [512, 256, 10], torch.nn.BatchNorm1d, torch.nn.ReLU, True, 0.1)
-
-student_opt = torch.optim.SGD(student.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-5)
-teacher_opt = torch.optim.SGD(teacher.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-5)
-
+# their optimizers
+student_opt = torch.optim.SGD(student.parameters(), lr=0.1, momentum=0.1, weight_decay=1e-5)
+teacher_opt = torch.optim.SGD(teacher.parameters(), lr=0.1, momentum=0.1, weight_decay=1e-5)
+# train for 100 MPL weight updates, more is better but improvement beyond trivial accuracy is clearly observable in 100
 for i, ((U, _), (L, y)) in enumerate(zip(train, val)):
     out = MPL(U, L, y, student, teacher, student_opt, teacher_opt)
     if i % 10 == 0:
--- mpl.py
+++ mpl.py
@@ -4,41 +4,41 @@ import torch
 
 def MPL(U, L, Y, student, teacher, student_optimizer, teacher_optimizer, loss=torch.nn.CrossEntropyLoss(), sup_teacher=False):
     SPL = teacher(U) # compute the soft pseudo labels
-    PL = torch.tensor([np.random.choice(np.arange(SPL.shape[-1]),None,False, torch.nn.Softmax(-1)(SPL).detach().cpu().numpy()[xi]) for xi in range(SPL.shape[0])]) # sample from the SPL distribution
-    
+    PL = torch.tensor([np.random.choice(np.arange(SPL.shape[-1]), None, False, torch.nn.Softmax(-1)(SPL).detach().cpu().numpy()[xi]) for xi in range(SPL.shape[0])]) # sample from the SPL distribution
+    # compute the gradient of the student parameters with respect to the pseudo labels
     student_initial_output = student(U)
     student_loss_initial = loss(student_initial_output, PL)
     
     student_optimizer.zero_grad()
     student_loss_initial.backward()
     grads1 = [param.grad.data.detach().clone() for param in student.parameters()]
-
+    # update the student parameters based on pseudo labels from teacher
     torch.nn.utils.clip_grad_norm_(student.parameters(), 1.0)
     student_optimizer.step()
     
     student_optimizer.zero_grad()
-    
+    # compute the gradient of the student parameters with respect to the real labels
     student_final_output = student(L)
     student_loss_final = loss(student_final_output, Y)
     student_loss_final.backward()
     grads2 = [param.grad.data.detach().clone() for param in student.parameters()]
 
     student_optimizer.zero_grad()
-    
+    # compute h
     h = sum([(grad1 * grad2).sum() for grad1, grad2 in zip(grads1, grads2)])
-
+    # compute the teacher MPL loss
     teacher_loss_mpl = h * loss(SPL, PL)
     teacher_out = teacher(L)
-    teacher_loss_sup = loss(teacher_out, Y)
-
+    # optionally compute the teacher's supervised loss
     if sup_teacher:
+        teacher_loss_sup = loss(teacher_out, Y)
         teacher_loss = teacher_loss_mpl + teacher_loss_sup
     else:
         teacher_loss = teacher_loss_mpl
-
+    # update teacher based on student performance
     teacher_optimizer.zero_grad()
     torch.nn.utils.clip_grad_norm_(teacher.parameters(), 1.0)
     teacher_loss.backward()
     teacher_optimizer.step()
-
+    # return the student output for the batch
     return student_final_output
